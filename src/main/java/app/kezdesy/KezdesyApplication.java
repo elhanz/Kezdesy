5f1b81375d5fb68611993feabe548cf1be3f3280
elhanz <201338@astanait.edu.kz>
2023-04-06 00:14:12 +0600

    Initial commit

---
+++ src/main/java/app/kezdesy/KezdesyApplication.java
@@ -0,0 +1,13 @@
+package app.kezdesy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KezdesyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KezdesyApplication.class, args);
+	}
+
+}
