4d95301b9cfee3656ef7e30e64e19021b5202c77
Ayabek <Aitkazinn03@gmail.com>
2023-05-16 16:58:07 +0600

    Adaptive page for Rooms, Profile

--- src/main/java/app/kezdesy/controller/ChatController.java
+++ src/main/java/app/kezdesy/controller/ChatController.java
@@ -10,14 +10,17 @@ import app.kezdesy.service.implementation.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.core.AbstractDestinationResolvingMessagingTemplate;
 import org.springframework.messaging.handler.annotation.*;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/chats")
@@ -33,6 +36,9 @@ public class ChatController {
 
     @Autowired
     private MessageRepository chatMessageRepo;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
     public ChatController(UserServiceImpl userService) {
         this.userService = userService;
@@ -88,5 +94,25 @@ public class ChatController {
         return message;
     }
 
-
+    @MessageMapping("/chat.deleteMessage/{chatId}")
+    @SendTo("/topic/{chatId}/messageDeleted")
+    public Long deleteMessage(@DestinationVariable Long chatId, @Payload Long messageId) {
+        Room room = roomRepository.findById(chatId).orElse(null);
+        if (room != null) {
+            // Find the message in the room's message list
+            Optional<Message> optionalMessage = room.getMessages().stream()
+                    .filter(message -> message.getId().equals(messageId))
+                    .findFirst();
+            if (optionalMessage.isPresent()) {
+                Message message = optionalMessage.get();
+                // Update the message content
+                message.setContent("Message was deleted");
+                // Save the modified message
+                chatMessageRepo.save(message);
+                // Return the deleted message ID
+                return messageId;
+            }
+        }
+        return null;
+    }
 }
